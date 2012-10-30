0d7af4134a0cee83818dcdb44ba58a3d660f566c
Elias <eparalikis@yahoo.de>
2012-10-30 16:42:01 +0100

    Commit fix 2

--- MineAndroidSweeper/gen/edu/hm/mineandroidsweeper/R.java
+++ MineAndroidSweeper/gen/edu/hm/mineandroidsweeper/R.java
@@ -18,7 +18,8 @@ public final class R {
         public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
