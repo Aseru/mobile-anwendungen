af17ef61d3c764b91043c747105a034733c9209f
fake3 <fents@gmx.net>
2012-11-06 15:59:31 +0100

    removed click listeners from layout added click listeners in java code added intent to start a new game removed hard coded strings

--- MineAndroidSweeper/gen/edu/hm/mineandroidsweeper/R.java
+++ MineAndroidSweeper/gen/edu/hm/mineandroidsweeper/R.java
@@ -28,15 +28,20 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040003;
-        public static final int hello_world=0x7f040004;
-        public static final int highscore=0x7f040002;
-        public static final int menu_settings=0x7f040005;
+        public static final int app_name=0x7f040006;
+        public static final int hello_world=0x7f040007;
+        public static final int highscore=0x7f040005;
+        public static final int menu_settings=0x7f040008;
         /**  TextViews 
          */
-        public static final int new_game=0x7f040000;
-        public static final int resume=0x7f040001;
-        public static final int title_activity_main=0x7f040006;
+        public static final int new_game=0x7f040003;
+        public static final int resume=0x7f040004;
+        public static final int str_dbg_highscore_clicked=0x7f040001;
+        /**  MainMenuActivity 
+         */
+        public static final int str_dbg_new_game_clicked=0x7f040000;
+        public static final int str_dbg_resume_clicked=0x7f040002;
+        public static final int title_activity_main=0x7f040009;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
