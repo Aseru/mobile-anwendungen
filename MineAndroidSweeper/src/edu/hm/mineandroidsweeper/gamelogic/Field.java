4fd2b1d2a2573ec80af63b6fcaf526eac3265e52
fake3 <fents@gmx.net>
2012-11-09 16:08:55 +0100

    implemented field

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/gamelogic/Field.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/gamelogic/Field.java
@@ -1,9 +1,63 @@
 package edu.hm.mineandroidsweeper.gamelogic;
 
 public class Field {
+	
+	private final int xCoord;
+	private final int yCoord;
+	
+	private boolean isBomb;
+	private int value;
 
-	public Field() {
-		// TODO Auto-generated constructor stub
+	public Field(int x, int y) {
+		this.xCoord = x;
+		this.yCoord = y;
+		
+		isBomb = false;
+		value = -1;
 	}
+
+	/**
+	 * @return the isBomb
+	 */
+	public boolean isBomb() {
+		return isBomb;
+	}
+
+	/**
+	 * @param isBomb the isBomb to set
+	 */
+	public void setBomb(boolean isBomb) {
+		this.isBomb = isBomb;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the xCoord
+	 */
+	public int getxCoord() {
+		return xCoord;
+	}
+
+	/**
+	 * @return the yCoord
+	 */
+	public int getyCoord() {
+		return yCoord;
+	}
+	
+	
 
 }
