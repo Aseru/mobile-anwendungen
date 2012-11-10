ff87d0be7bb2d590fa5fd9ed2132ff8404a19320
fake3 <fents@gmx.net>
2012-11-10 20:39:34 +0100

    Implemented Flood Fill algorithm Implemented test for fill8()

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/gamelogic/FloodFill.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/gamelogic/FloodFill.java
@@ -1,9 +1,22 @@
 package edu.hm.mineandroidsweeper.gamelogic;
 
+import java.util.Map;
+
 public class FloodFill {
 
-	public FloodFill() {
-		// TODO Auto-generated constructor stub
+	public static void fill8(Map<Coordinate, Field> map, Field start) {
+		if (start == null || start.isBomb() || start.isRevealed()) {
+			return;
+		}
+		start.setRevealed(true);
+		if (start.getValue() > 0) {
+			return;
+		}
+		
+		Field[] neighborFields = start.getNeighborFields8(map);
+		for(Field field : neighborFields){
+			fill8(map, field);
+		}
 	}
 
 }
---
+++ MineAndroidSweeperTest/src/edu/hm/mineandroidsweeper/test/gamelogic/FloodFillTest.java
@@ -0,0 +1,54 @@
+package edu.hm.mineandroidsweeper.test.gamelogic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+import edu.hm.mineandroidsweeper.gamelogic.Coordinate;
+import edu.hm.mineandroidsweeper.gamelogic.Field;
+import edu.hm.mineandroidsweeper.gamelogic.FloodFill;
+
+public class FloodFillTest extends TestCase {
+	
+
+	public void testFloodFill8() throws Exception {
+		Map<Coordinate, Field> map = new HashMap<Coordinate, Field>();
+		
+		Field[] expectedfields = new Field[] { 
+				new Field(new Coordinate(1, 1)),
+				new Field(new Coordinate(2, 1)),
+				new Field(new Coordinate(3, 1)),
+				new Field(new Coordinate(1, 2)),
+				new Field(new Coordinate(2, 2)),
+				new Field(new Coordinate(3, 2)),
+				new Field(new Coordinate(1, 3)),
+				new Field(new Coordinate(2, 3)),
+				new Field(new Coordinate(3, 3)) };
+		
+		expectedfields[0].setBomb(true);
+		expectedfields[1].increaseValue();
+		expectedfields[3].increaseValue();
+		expectedfields[4].increaseValue();
+		
+		for (Field f : expectedfields) {
+			map.put(f.getCoord(), f);
+		}
+		
+		FloodFill.fill8(map, expectedfields[expectedfields.length-1]);
+		
+		assertTrue(expectedfields[1].isRevealed());
+		assertTrue(expectedfields[2].isRevealed());
+		assertTrue(expectedfields[3].isRevealed());
+		assertTrue(expectedfields[4].isRevealed());
+		assertTrue(expectedfields[5].isRevealed());
+		assertTrue(expectedfields[6].isRevealed());
+		assertTrue(expectedfields[7].isRevealed());
+		assertTrue(expectedfields[8].isRevealed());
+		
+		assertFalse(expectedfields[0].isRevealed());
+	}
+
+	
+	
+
+}
