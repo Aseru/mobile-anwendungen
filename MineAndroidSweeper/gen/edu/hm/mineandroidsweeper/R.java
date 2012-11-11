ced690452b96a5beb1012266a66fbe4bc82bbdb3
fake3 <fents@gmx.net>
2012-11-11 01:38:08 +0100

    some tests with Chronometer

--- MineAndroidSweeper/gen/edu/hm/mineandroidsweeper/R.java
+++ MineAndroidSweeper/gen/edu/hm/mineandroidsweeper/R.java
@@ -15,10 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070003;
-        public static final int textView_highscore=0x7f070002;
-        public static final int textView_newGame=0x7f070000;
-        public static final int textView_resume=0x7f070001;
+        public static final int chronometer=0x7f070000;
+        public static final int menu_settings=0x7f070004;
+        public static final int textView_highscore=0x7f070003;
+        public static final int textView_newGame=0x7f070001;
+        public static final int textView_resume=0x7f070002;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/activities/GameActivity.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/activities/GameActivity.java
@@ -1,12 +1,21 @@
 package edu.hm.mineandroidsweeper.activities;
 
-import edu.hm.mineandroidsweeper.R;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.Chronometer;
+import android.widget.Chronometer.OnChronometerTickListener;
+import edu.hm.mineandroidsweeper.R;
 
 public class GameActivity extends Activity {
 	
 	public static final String TAG = "GameActicity";
+	
+	private Chronometer chrono;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,42 @@ public class GameActivity extends Activity {
 	
 	private void initView(Bundle savedInstanceState){
 		setContentView(R.layout.activity_game);
+		chrono = (Chronometer) findViewById(R.id.chronometer);
+		chrono.setBase(android.os.SystemClock.elapsedRealtime());
+		chrono.setOnChronometerTickListener(new OnChronometerTickListener() {
+			
+			long base;
+			long current;
+			long time;
+			
+			public void onChronometerTick(Chronometer chronometer) {
+				base = chronometer.getBase();
+				current = android.os.SystemClock.elapsedRealtime();
+				time = current - base;
+				chronometer.setText(Long.toString(TimeUnit.MILLISECONDS.toSeconds(time)));
+			}
+		});
+		chrono.start();
 	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
+	@Override
+	protected void onPause() {
+		chrono.stop();
+		super.onPause();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		chrono.start();
+	}
+	
+	
 
 }
