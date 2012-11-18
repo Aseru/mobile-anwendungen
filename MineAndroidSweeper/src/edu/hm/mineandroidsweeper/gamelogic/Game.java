2eb1a304003acd52a3f841e0ad6dcdd37013bf84
fake3 <fents@gmx.net>
2012-11-18 01:03:16 +0100

    renamed performClick() to reveal() implemented reveal() (not complete)

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/gamelogic/Game.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/gamelogic/Game.java
@@ -2,40 +2,46 @@ package edu.hm.mineandroidsweeper.gamelogic;
 
 import java.io.Serializable;
 
+import android.view.View;
 import edu.hm.mineandroidsweeper.difficulties.IDifficulty;
 
 public class Game implements Serializable {
-	
+
 	public static final String EXTRA_NAME = "edu.hm.mineandroidsweeper.extra.game";
-	
+
 	private static final long serialVersionUID = 7823809805228633799L;
 
 	private final IDifficulty difficulty;
-	
+
 	private GameState state;
 	private Playground playground;
 	private long currentPlaytime;
-	
+
 	/* No-args constructor needed for Serialization. */
 	protected Game() {
 		difficulty = null;
 	}
-	
+
 	public Game(IDifficulty difficulty) {
 		this.difficulty = difficulty;
 	}
-	
-	public void init(){
+
+	public void init() {
 		state = GameState.STARTING;
 		playground = new Playground(this, difficulty);
 		playground.init();
 	}
-	
-	public void performClick(){
-		if(state != GameState.RUNNING){
+
+	public void reveal(View view) {
+		if (state != GameState.RUNNING) {
 			return;
 		}
-		
+		Object tag = view.getTag();
+		Field field;
+		if (tag instanceof Field) {
+			field = (Field) tag;
+			playground.reveal(field);
+		}
 	}
 
 	/**
@@ -46,7 +52,8 @@ public class Game implements Serializable {
 	}
 
 	/**
-	 * @param state the state to set
+	 * @param state
+	 *            the state to set
 	 */
 	public void setState(GameState state) {
 		this.state = state;
@@ -67,14 +74,11 @@ public class Game implements Serializable {
 	}
 
 	/**
-	 * @param currentPlaytime the currentPlaytime to set
+	 * @param currentPlaytime
+	 *            the currentPlaytime to set
 	 */
 	public void setCurrentPlaytime(long currentPlaytime) {
 		this.currentPlaytime = currentPlaytime;
 	}
-	
-	
-	
-	
 
 }
