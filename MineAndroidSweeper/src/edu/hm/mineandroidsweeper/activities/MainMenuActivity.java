25ab716cf328868fb2b8c5d65149f73146dc753a
Elias <eparalikis@yahoo.de>
2012-11-20 16:34:41 +0100

    - In der MainActivity xml onClick für die Buttons definiert - Config Dateien für die Code conventions tools hinzugefügt (Checkstyle, PMD, Findbugs)

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/activities/MainMenuActivity.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/activities/MainMenuActivity.java
@@ -7,10 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 import edu.hm.mineandroidsweeper.R;
 import edu.hm.mineandroidsweeper.gamelogic.Game;
 import edu.hm.mineandroidsweeper.persistence.GameLoader;
@@ -22,43 +19,13 @@ public class MainMenuActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initView();
-	}
-
-	private void initView() {
 		setContentView(R.layout.activity_main);
-
-		TextView newGame = (TextView) findViewById(R.id.textView_newGame);
-		newGame.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				onNewGamesClicked(v);
-			}
-		});
-
-		TextView resume = (TextView) findViewById(R.id.textView_resume);
-		resume.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				onResumeClicked(v);
-			}
-		});
-
-		TextView highscore = (TextView) findViewById(R.id.textView_highscore);
-		highscore.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				onHighscoreClicked(v);
-			}
-		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-	private void onResumeClicked(View view) {
+	
+	
+	// Click Methods
+	public void onResumeClicked(View view) {
 		Log.d(TAG, getString(R.string.str_dbg_resume_clicked));
 		Intent intent = new Intent(this, GameActivity.class);
 		Game loadedGame = loadGame();
@@ -68,16 +35,17 @@ public class MainMenuActivity extends Activity {
 		startActivity(intent);
 	}
 
-	private void onNewGamesClicked(View view) {
+	public void onNewGamesClicked(View view) {
 		Log.d(TAG, getString(R.string.str_dbg_new_game_clicked));
 		Intent intent = new Intent(this, DifficultActivity.class);
 		startActivity(intent);
 	}
 
-	private void onHighscoreClicked(View view) {
+	public void onHighscoreClicked(View view) {
 		Log.d(TAG, getString(R.string.str_dbg_highscore_clicked));
 	}
 
+	
 	private Game loadGame() {
 		Game loadedGame;
 
