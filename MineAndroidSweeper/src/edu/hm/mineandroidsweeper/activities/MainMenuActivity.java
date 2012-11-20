fee4d643ec63d886afcb815ce735d629dcc9afd1
Elias <eparalikis@yahoo.de>
2012-11-20 17:27:23 +0100

    - Save game fixed: View in Field marked as transient, however still doesn't work: when resuming it does not display the old game

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/activities/MainMenuActivity.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/activities/MainMenuActivity.java
@@ -13,51 +13,54 @@ import edu.hm.mineandroidsweeper.gamelogic.Game;
 import edu.hm.mineandroidsweeper.persistence.GameLoader;
 
 public class MainMenuActivity extends Activity {
-
-	public static final String TAG = "MainMenuActivity";
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	
-	
-	// Click Methods
-	public void onResumeClicked(View view) {
-		Log.d(TAG, getString(R.string.str_dbg_resume_clicked));
-		Intent intent = new Intent(this, GameActivity.class);
-		Game loadedGame = loadGame();
-		if (loadedGame != null) {
-			intent.putExtra(Game.EXTRA_NAME, loadedGame);
-		}
-		startActivity(intent);
-	}
-
-	public void onNewGamesClicked(View view) {
-		Log.d(TAG, getString(R.string.str_dbg_new_game_clicked));
-		Intent intent = new Intent(this, DifficultActivity.class);
-		startActivity(intent);
-	}
-
-	public void onHighscoreClicked(View view) {
-		Log.d(TAG, getString(R.string.str_dbg_highscore_clicked));
-	}
-
-	
-	private Game loadGame() {
-		Game loadedGame;
-
-		FileInputStream fileInputStream;
-		try {
-			fileInputStream = openFileInput(GameLoader.SAVE_GAME_FILENAME);
-		} catch (FileNotFoundException e) {
-			return null;
-		}
-
-		loadedGame = GameLoader.loadGame(fileInputStream);
-		return loadedGame;
-	}
-
+    
+    public static final String TAG = "MainMenuActivity";
+    
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    
+    
+    // Click Methods
+    public void onResumeClicked(final View view) {
+        Log.d(TAG, getString(R.string.str_dbg_resume_clicked));
+        Intent intent = new Intent(this, GameActivity.class);
+        Game loadedGame = loadGame();
+        if(loadedGame==null) {
+            // Kein gespeichertes Spiel gefunden
+            return;
+        }
+        
+        intent.putExtra(Game.EXTRA_NAME, loadedGame);
+        startActivity(intent);
+    }
+    
+    public void onNewGamesClicked(final View view) {
+        Log.d(TAG, getString(R.string.str_dbg_new_game_clicked));
+        Intent intent = new Intent(this, DifficultActivity.class);
+        startActivity(intent);
+    }
+    
+    public void onHighscoreClicked(final View view) {
+        Log.d(TAG, getString(R.string.str_dbg_highscore_clicked));
+    }
+    
+    
+    private Game loadGame() {
+        Game loadedGame;
+        
+        FileInputStream fileInputStream;
+        try {
+            fileInputStream = openFileInput(GameLoader.SAVE_GAME_FILENAME);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+        
+        loadedGame = GameLoader.loadGame(fileInputStream);
+        return loadedGame;
+    }
+    
 }
