d0d33ed814f1beb852fd41a92f5e0de34ab9bac4
fake3 <fents@gmx.net>
2012-11-23 13:48:02 +0100

    adapt save and load tests to refactored GameLoader

--- MineAndroidSweeperTest/src/edu/hm/mineandroidsweeper/persistence/GameLoaderTest.java
+++ MineAndroidSweeperTest/src/edu/hm/mineandroidsweeper/persistence/GameLoaderTest.java
@@ -1,9 +1,43 @@
 package edu.hm.mineandroidsweeper.persistence;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OptionalDataException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
 import junit.framework.TestCase;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.IntentSender.SendIntentException;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.content.res.Resources.Theme;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.test.mock.MockContext;
+import edu.hm.mineandroidsweeper.activities.GameActivity;
 import edu.hm.mineandroidsweeper.difficulties.EasyDifficulty;
 import edu.hm.mineandroidsweeper.difficulties.IDifficulty;
 import edu.hm.mineandroidsweeper.gamelogic.Game;
@@ -21,23 +55,22 @@ public class GameLoaderTest extends TestCase {
 	public void setUp() throws Exception {		
 		outputStream = new PipedOutputStream();
 		inputStream = new PipedInputStream(outputStream);
-		
 		difficulty = new EasyDifficulty();
 		game = new Game(difficulty);
+		game.setState(GameState.STARTING);
 	}
 	
-	public void testLoadAndSave() {
-		saveGame();
-		loadGame();
+	public void testLoadAndSave() throws Exception {
+			saveGame();
+			loadGame();
 	}
 	
-	public void saveGame() {
-		boolean success = GameLoader.saveGame(game, outputStream);
-		assertTrue(success);
+	private void saveGame() throws IOException {
+		GameLoader.saveObject(outputStream, game);
 	}
 	
-	public void loadGame() {
-		Game loadedGame = GameLoader.loadGame(inputStream);
+	private void loadGame() throws OptionalDataException, ClassNotFoundException, IOException {
+		Game loadedGame = (Game) GameLoader.loadObject(inputStream);
 		assertNotNull(loadedGame);
 		
 		// Test if loadedGame equals game.
