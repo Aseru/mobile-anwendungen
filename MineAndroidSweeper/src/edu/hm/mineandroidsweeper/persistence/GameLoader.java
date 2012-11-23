816c7218f917870e648a4fa3900f165f4a493d47
fake3 <fents@gmx.net>
2012-11-23 13:43:38 +0100

    refactored GameLoader for more flexibility

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/persistence/GameLoader.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/persistence/GameLoader.java
@@ -1,60 +1,83 @@
 package edu.hm.mineandroidsweeper.persistence;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OptionalDataException;
 import java.io.OutputStream;
 
+import android.content.Context;
+import android.util.Log;
 import edu.hm.mineandroidsweeper.gamelogic.Game;
 import edu.hm.mineandroidsweeper.gamelogic.GameState;
 
 public final class GameLoader {
-	
-	public static final String SAVE_GAME_FILENAME = "ms_savegame_data.ser";	
-
-	
-	public static boolean saveGame(Game gameToSave, OutputStream outputStream) {
-		GameState currentState = gameToSave.getState();
-		
-		gameToSave.setState(GameState.SAVED);
-		ObjectOutputStream objectOutputStream = null;
-		
-		try {
-			objectOutputStream = new ObjectOutputStream(outputStream);
-			objectOutputStream.writeObject(gameToSave);
-		}
-		catch (IOException e) {
-			gameToSave.setState(currentState);
-			return false;
-		} finally {
-			try {
-				objectOutputStream.close();
-			} catch (IOException e) { }
-		}
-		return true;
-	}
-	
-	public static Game loadGame(InputStream inputStream) {
-		Game loadedGame;
-		ObjectInputStream objectInputStream = null;
-		try {
-			objectInputStream = new ObjectInputStream(inputStream);
-			
-			loadedGame = (Game) objectInputStream.readObject();
-		} catch (IOException e) {
-			return null;
-		} catch (ClassNotFoundException e) {
-			return null;
-		}
-		finally {
-			try {
-				objectInputStream.close();
-			} catch (IOException e) {}
-		}
-		
-		loadedGame.setState(GameState.STARTING);
-		return loadedGame;
-	}
-	
+    
+    public static final String TAG = "GameLoader";
+    public static final String SAVE_GAME_FILENAME = "ms_savegame_data.ser";
+    
+    public static boolean saveGame(final Context context, final Game game) {
+        FileOutputStream fileOutputStream = null;
+        GameState currentState = game.getState();
+        
+        try {
+            fileOutputStream = context.openFileOutput(GameLoader.SAVE_GAME_FILENAME,
+                    Context.MODE_PRIVATE);
+            saveObject(fileOutputStream, game);
+            game.setState(GameState.SAVED);
+        }
+        catch (FileNotFoundException e) {
+            Log.e(TAG, "Exception:", e);
+            game.setState(currentState);
+            return false;
+        }
+        catch (IOException e) {
+            Log.e(TAG, "Exception:", e);
+            game.setState(currentState);
+            return false;
+        }
+        return true;
+    }
+    
+    public static void saveObject(final OutputStream outStream, final Object object)
+            throws IOException {
+        ObjectOutputStream objectOutputStream = null;
+        objectOutputStream = new ObjectOutputStream(outStream);
+        objectOutputStream.writeObject(object);
+        objectOutputStream.close();
+    }
+    
+    public static Object loadObject(final InputStream inStream) throws OptionalDataException,
+    ClassNotFoundException, IOException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(inStream);
+        Object object = objectInputStream.readObject();
+        objectInputStream.close();
+        return object;
+    }
+    
+    public static Game loadGame(final Context context) {
+        Game loadedGame = null;
+        FileInputStream fileInputStream = null;
+        
+        Object object;
+        try {
+            fileInputStream = context.openFileInput(SAVE_GAME_FILENAME);
+            object = loadObject(fileInputStream);
+            loadedGame = (Game) object;
+        }
+        catch (OptionalDataException e) {
+            Log.e(TAG, "Exception:", e);
+        }
+        catch (ClassNotFoundException e) {
+            Log.e(TAG, "Exception:", e);
+        }
+        catch (IOException e) {
+            Log.e(TAG, "Exception:", e);
+        }
+        return loadedGame;
+    }
 }
