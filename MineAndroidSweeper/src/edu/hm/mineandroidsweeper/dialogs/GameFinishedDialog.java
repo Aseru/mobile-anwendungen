cf845e236f4642417c41b7550f89487c7f71c44c
fake3 <fents@gmx.net>
2012-12-04 14:03:52 +0100

    added buttons to gamefinishdialog added icons for bomb and flag in the info bar

--- MineAndroidSweeper/src/edu/hm/mineandroidsweeper/dialogs/GameFinishedDialog.java
+++ MineAndroidSweeper/src/edu/hm/mineandroidsweeper/dialogs/GameFinishedDialog.java
@@ -2,13 +2,13 @@ package edu.hm.mineandroidsweeper.dialogs;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import edu.hm.mineandroidsweeper.R;
 
 /**
  * TODO: Document type GameFinishedDialog.
  */
 public class GameFinishedDialog extends AlertDialog {
-    
     
     private final Context context;
     
@@ -19,10 +19,10 @@ public class GameFinishedDialog extends AlertDialog {
     }
     
     private void init(final boolean isWon, final double time) {
-        setCancelable(true);
+        setCancelable(false);
         String title;
         String message;
-        if(isWon){
+        if (isWon) {
             title = context.getString(R.string.dialog_finished_won_title);
             message = context.getString(R.string.dialog_finished_won_message, time);
             setIcon(R.drawable.smily_happy);
@@ -32,9 +32,39 @@ public class GameFinishedDialog extends AlertDialog {
             message = context.getString(R.string.dialog_finished_lose_message, time);
             setIcon(R.drawable.smily_sad);
         }
+        
+        createButtons();
         setTitle(title);
         setMessage(message);
     }
     
+    private void createButtons() {
+        setButton(BUTTON_NEGATIVE, context.getString(R.string.dialog_finished_button_back),
+                new OnClickListener() {
+            
+            @Override
+            public void onClick(final DialogInterface dialog, final int which) {
+                dialog.dismiss();
+            }
+        });
+        setButton(BUTTON_NEUTRAL, context.getString(R.string.dialog_finished_button_highscore),
+                new OnClickListener() {
+            
+            @Override
+            public void onClick(final DialogInterface dialog, final int which) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        setButton(BUTTON_POSITIVE, context.getString(R.string.dialog_finished_button_restart),
+                new OnClickListener() {
+            
+            @Override
+            public void onClick(final DialogInterface dialog, final int which) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+    }
+    
 }
-
