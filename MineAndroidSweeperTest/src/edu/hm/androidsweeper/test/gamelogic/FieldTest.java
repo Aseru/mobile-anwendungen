3f0bd503ba290934679b0c22ec16c7b320be6bfb
Elias <eparalikis@yahoo.de>
2013-01-07 22:46:31 +0100

    Fixed preference strings. Removed unused test method.

--- MineAndroidSweeperTest/src/edu/hm/androidsweeper/test/gamelogic/FieldTest.java
+++ MineAndroidSweeperTest/src/edu/hm/androidsweeper/test/gamelogic/FieldTest.java
@@ -16,22 +16,6 @@ public class FieldTest extends TestCase {
 		assertEquals(field1, field2);
 	}
 
-//	public void testGetNeighborCoords8() {
-//		Coordinate coord = new Coordinate(2, 2);
-//		Field field = new Field(coord);
-//		Coordinate[] coords = new Coordinate[] { new Coordinate(1, 1),
-//				new Coordinate(2, 1), new Coordinate(3, 1),
-//				new Coordinate(1, 2), new Coordinate(3, 2),
-//				new Coordinate(1, 3), new Coordinate(2, 3),
-//				new Coordinate(3, 3) };
-//
-//		Coordinate[] neighbors = field.getNeighborCoords8();
-//
-//		for (int i = 0; i < coords.length; i++) {
-//			assertEquals(coords[i], neighbors[i]);
-//		}
-//	}
-
 	public void testIncreaseValue() {
 		Field bomb = new Field(new Coordinate(2, 2));
 		bomb.setBomb(true);
